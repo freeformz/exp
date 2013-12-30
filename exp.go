3b258e4b7784236be657a372b84a705756101580
Edward Muller <edwardam@interlix.com>
2013-12-29 18:31:46 -0800

    .

--- exp.go
+++ exp.go
@@ -2,34 +2,73 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"sync/atomic"
+	"time"
 )
 
 func main() {
 	listen := os.Getenv("LISTEN")
+	debug := os.Getenv("DEBUG") != ""
+
+	var drops uint64
 
 	if listen == "" {
 		listen = ":8080"
 	}
 
+	fmt.Println("Listening on: " + listen)
+
+	if !debug {
+		go func() {
+			ticker := time.Tick(5 * time.Second)
+			for {
+				select {
+				case <-ticker:
+					fmt.Println("Drops: ", drops)
+				}
+			}
+		}()
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(time.Now())
-		fmt.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
-		fmt.Println("Headers:")
-		for k, _ := range r.Header {
-			fmt.Printf("\t%s: %s\n", k, r.Header.Get(k))
+		if debug {
+			fmt.Println(time.Now())
+			fmt.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
+			fmt.Println("Headers:")
 		}
-		fmt.Printf("Content Length: %d\n", r.ContentLength)
+		rdrops := r.Header.Get("Logshuttle-Drops")
+		d, err := strconv.Atoi(rdrops)
+		if err == nil {
+			atomic.AddUint64(&drops, uint64(d))
+		}
+		if debug {
+			for k, _ := range r.Header {
+				fmt.Printf("\t%s: %s\n", k, r.Header.Get(k))
+			}
+		}
+		if debug {
+			fmt.Printf("Content Length: %d\n", r.ContentLength)
+		}
+
 		if r.ContentLength > 0 {
-			var body = make([]byte, r.ContentLength)
-			r.Body.Read(body)
-			fmt.Println("Body:")
-			fmt.Println(string(body))
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Println("Error Reading Body:", err)
+			} else {
+				if debug {
+					fmt.Println("Body:")
+					fmt.Println(string(body))
+				}
+			}
 		}
 		w.WriteHeader(http.StatusOK)
-		fmt.Println()
+		if debug {
+			fmt.Println()
+		}
 	})
 
 	http.ListenAndServe(listen, nil)
